d8e08cd8c7a035ca533e52a16fabe94e0b4f152e
Yezlion <go4yesi@hotmail.com>
2016-05-18 20:35:27 -0500

    Update

---
+++ Class5 Hmwrk.js
@@ -0,0 +1,23 @@
+/*
+In this file there are bugs. You must fix them.
+The bugs consist of syntax errors and logic errors.
+The correct output of the main function is: 24
+There are FIVE errors in this code.
+*/
+
+function main () {
+	var x = 1;
+	var y = 2;
+
+	z = x + y;
+	q = z;
+
+	for (var i = 0; i > z; i++) {
+		q = q * 2;
+	}
+
+	return q;
+}
+
+
+console.log(main());
